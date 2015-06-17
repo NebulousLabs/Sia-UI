66517d5dbff031f6a60514f4afa97c8d4bfc6fd0
Mingling94 <mcluo1994@gmail.com>
2015-06-17 12:40:36 -0400

    Linted some code, no functional difference

--- site/js/viewLoader.js
+++ site/js/viewLoader.js
@@ -1,28 +1,31 @@
 // viewLoader manages which view is currently being displayed in the mainbar of
 // the UI.
+var window = null;
 
 window.onload = function() {
+	var view = window.$('#view'),
+		sidebarOverviewButton = window.document.getElementById('sidebar-overview-button'),
+		sidebarLibraryButton = window.document.getElementById('sidebar-library-button'),
+		sidebarWalletButton = window.document.getElementById('sidebar-wallet-button');
+
 	// Default to the 'Overview' view.
-	$('#view').load('overview.html');
+	view.load('overview.html');
 
 	// Enable the overview button.
-	var sidebarOverviewButton = document.getElementById('sidebar-overview-button');
 	sidebarOverviewButton.style.cursor = 'pointer';
-	sidebarOverviewButton.onclick = function(){
-		$('#view').load('overview.html');
+	sidebarOverviewButton.onclick = function() {
+		view.load('overview.html');
 	};
 
 	// Enable the library button.
-	var sidebarLibraryButton = document.getElementById('sidebar-library-button');
 	sidebarLibraryButton.style.cursor = 'pointer';
-	sidebarLibraryButton.onclick = function(){
-		$('#view').load('library.html');
+	sidebarLibraryButton.onclick = function() {
+		view.load('library.html');
 	};
 
 	// Enable the wallet button.
-	var sidebarWalletButton = document.getElementById('sidebar-wallet-button');
 	sidebarWalletButton.style.cursor = 'pointer';
-	sidebarWalletButton.onclick = function(){
-		$('#view').load('wallet.html');
+	sidebarWalletButton.onclick = function() {
+		view.load('wallet.html');
 	};
-}
+};
