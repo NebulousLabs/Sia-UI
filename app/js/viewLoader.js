d9d38a7b3312eb55aad40910b28e849ceaf0aa8e
Mingling94 <mcluo1994@gmail.com>
2015-06-18 15:19:15 -0400

    moved index.html out of app/html/ to app/
    Adheres to electron standards

--- app/js/viewLoader.js
+++ app/js/viewLoader.js
@@ -3,26 +3,26 @@
 
 window.onload = function() {
 	// Default to the 'Overview' view.
-	$('#view').load('overview.html');
+	$('#view').load('html/overview.html');
 
 	// Enable the overview button.
 	var sidebarOverviewButton = document.getElementById('sidebar-overview-button');
 	sidebarOverviewButton.style.cursor = 'pointer';
 	sidebarOverviewButton.onclick = function(){
-		$('#view').load('overview.html');
+		$('#view').load('html/overview.html');
 	};
 
 	// Enable the library button.
 	var sidebarLibraryButton = document.getElementById('sidebar-library-button');
 	sidebarLibraryButton.style.cursor = 'pointer';
 	sidebarLibraryButton.onclick = function(){
-		$('#view').load('library.html');
+		$('#view').load('html/library.html');
 	};
 
 	// Enable the wallet button.
 	var sidebarWalletButton = document.getElementById('sidebar-wallet-button');
 	sidebarWalletButton.style.cursor = 'pointer';
 	sidebarWalletButton.onclick = function(){
-		$('#view').load('wallet.html');
+		$('#view').load('html/wallet.html');
 	};
 }
