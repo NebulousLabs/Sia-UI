46ed06c35c93c0297bf3af78676552f778fb0f01
Mingling94 <mcluo1994@gmail.com>
2015-07-07 17:30:23 -0400

    makefile back to original state. code reorganized

--- app/plugins/Overview/index.js
+++ app/plugins/Overview/index.js
@@ -1,14 +1,18 @@
 // main.js, the entry point of the plugin, handles updating the Overview fields
 'use strict';
+const ipc = require('ipc');
 
 window.onload = function Overview() {
+	// var pointers to markup elements
 	var balance
 	var peers = document.getElement
 	var blockHeight
 
+	ipc.on('update', update);
 	function update() {
 		eBalance.innerHTML = data.wallet.Balance;
 		ePeers.innerHTML = "Peers: " + data.peer.Peers.length;
 		eBlockHeight.innerHTML = "Block Height: " + data.consensus.Height;
 	}
 };
+
