6926a4d49e60ccda2224d203c26178fc9c840472
Mingling94 <mcluo1994@gmail.com>
2015-08-03 11:42:11 -0400

    Renamed to Hosting, added barebones CSS

---
+++ app/plugins/Hosting/js/hosting.js
@@ -0,0 +1,31 @@
+'use strict';
+// Library for communicating with Sia-UI
+const IPC = require('ipc');
+// Library for arbitrary precision in numbers
+const BigNumber = require('bignumber.js');
+// Variables to store api result values
+var balance = 0;
+// Keeps track of if the view is shown
+var updating;
+
+function callAPI() {
+}
+
+function formatSiacoin(baseUnits) {
+	var ConversionFactor = new BigNumber(10).pow(24);
+	var display = new BigNumber(baseUnits).dividedBy(ConversionFactor);
+	return display + ' SC';
+}
+
+function init() {
+	// DEVTOOL: uncomment to bring up devtools on plugin view
+	//IPC.sendToHost('devtools');
+	
+	// Ensure precision
+	BigNumber.config({ DECIMAL_PLACES: 24 })
+	BigNumber.config({ EXPONENTIAL_AT: 1e+9 })
+}
+
+function kill() {
+	clearInterval(updating);
+}
