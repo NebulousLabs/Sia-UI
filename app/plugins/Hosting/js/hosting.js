52e60ea42491f017c101775f7b17c51fca8e6705
Mingling94 <mcluo1994@gmail.com>
2015-08-09 17:12:27 -0400

    Adapting hosting.js's old code to new structure
    Work in progress

--- app/plugins/Hosting/js/hosting.js
+++ app/plugins/Hosting/js/hosting.js
@@ -11,9 +11,10 @@ var listening = false;
 var eID = function() {
 	return document.getElementById.apply(document, [].slice.call(arguments));
 };
+var hostStatus;
 
 // Pointers to various DOM elements
-var view, ePropBlueprint, eProps, eControl, eSave, eReset, eAnnounce;
+var view, ePropBlueprint, eProperties, eControl, eSave, eReset, eAnnounce;
 var eContracts, eStorage, eRemaining, eProfit, ePotentialProfit;
 var updateTime = 0;
 
@@ -56,7 +57,80 @@ function callAPI(call, callback) {
 function formatSiacoin(baseUnits) {
 	var ConversionFactor = new BigNumber(10).pow(24);
 	var display = new BigNumber(baseUnits).dividedBy(ConversionFactor);
-	return display + ' SC';
+	return display + ' S';
+}
+
+// Controls data size representation
+function formatBytes(bytes) {
+	if (bytes == 0) return '0B';
+	var k = 1000;
+	var sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	var i = Math.log(bytes)/ Math.log(k);
+	return (bytes / Math.pow(k, i)).toPrecision(3) + " " + sizes[i];
+}
+
+// Ask UI to show tooltip bubble
+function tooltip(message, element) {
+	var rect = element.getBoundingClientRect();
+	IPC.sendToHost('tooltip', message, {
+		top: rect.top,
+		bottom: rect.bottom,
+		left: rect.left,
+		right: rect.right,
+		height: rect.height,
+		width: rect.width,
+		length: rect.length,
+	});
+};
+
+function addListeners(){
+	eAnnounce.onclick = function(){
+		tooltip('Anouncing...', this);
+		callAPI('/host/announce');
+	};
+	eSave.onclick = function(){
+		tooltip('Saving...', this);
+		// var call = {};
+		callAPI('/host/configure');
+	};
+	eReset.onclick = function(){
+		tooltip('Reseting...', this);
+		for (var i = 0; i < editableProps.length; i++){
+			var item = $(eProps[i]);
+			var value = parseFloat(ui._hostStatus[editableProps[i]]);
+			item.querySelectorAll('.value').textContent = util.round(value * propConversion[i]);
+		}
+	};
+}
+
+function parseHostInfo(){
+	var newInfo = {};
+	for (var i = 0; i < editableProps.length; i++){
+		var item = $(eProps[i]);
+		var value = parseFloat(item.querySelectorAll('.value').text());
+		newInfo[editableProps[i].toLowerCase()] = value / propConversion[i];
+	}
+	return newInfo;
+}
+
+function update(){
+	var total = formatBytes(hostStatus.TotalStorage);
+	var remaining = formatBytes(hostStatus.StorageRemaining);
+	var storage = formatBytes(hostStatus.TotalStorage - hostStatus.StorageRemaining);
+	var profit = hostStatus.Profit;
+	var potentialProfit = hostStatus.PotentialProfit;
+
+	eContracts.innerHTML = hostStatus.NumContracts + ' Contracts';
+	eStorage.innerHTML = storage + '/' + total + ' in use';
+	eRemaining.innerHTML = remaining + ' left';
+	eProfit.innerHTML = (util.siacoin(profit)).toFixed(4) + ' KS earned';
+	ePotentialProfit.innerHTML = (util.siacoin(potentialProfit)).toFixed(4) + ' KS to be earned';
+
+	var d = new Date();
+	if (updateTime < d.getTime() - 15000) {
+		document.getElementById('hmessage').innerHTML = 'Estimated Competitive Price: ' + 1000 * util.siacoin(hostStatus.Competition).toFixed(3) + ' SC / GB / Month';
+		updateTime = d.getTime();
+	}
 }
 
 function init() {
@@ -67,7 +141,9 @@ function init() {
 	BigNumber.config({ DECIMAL_PLACES: 24 })
 	BigNumber.config({ EXPONENTIAL_AT: 1e+9 })
 
+	// Assign DOM elements to var shortcuts
 	ePropBlueprint = eID('propertybp');
+	eProperties = eID('properties');
 	eAnnounce = eID('announce');
 	eSave = eID('save');
 	eReset = eID('reset');
@@ -76,7 +152,19 @@ function init() {
 	eProfit = eID('profit');
 	ePotentialProfit = eID('potentialprofit');
 
+	// Make buttons responsive
 	addListeners();
+
+	// Create and load all properties
+	hostProperties.forEach(function(prop) {
+		var item = ePropBlueprint.cloneNode(true).classList.remove('blueprint');
+		eProperties.appendChild(item);
+		item.querySelector('.name').textContent = prop.name + ' (' + prop.unit + ')';
+		var value = hostStatus[prop.name]);
+		item.querySelector('.value').textContent = value * prop.conversion;
+	});
+
+	// Start updating
 	updating = setInterval(update, 1000)
 }
 
@@ -84,63 +172,3 @@ function kill() {
 	clearInterval(updating);
 }
 
-function addListeners(){
-	eAnnounce.onclick = function(){
-		ui._trigger('announce-host');
-	};
-	eSave.onclick = function(){
-		// ui._tooltip(this, 'Saving');
-		ui._trigger('save-host-config', parseHostInfo());
-	};
-	eReset.onclick = function(){
-		ui._tooltip(this, 'Reseting');
-		for (var i = 0; i < editableProps.length; i++){
-			var item = $(eProps[i]);
-			var value = parseFloat(ui._data.host.HostInfo[editableProps[i]]);
-			item.find('.value').text(util.round(value * propConversion[i]));
-		}
-	};
-}
-
-function parseHostInfo(){
-	var newInfo = {};
-	for (var i = 0; i < editableProps.length; i++){
-		var item = $(eProps[i]);
-		var value = parseFloat(item.find('.value').text());
-		newInfo[editableProps[i].toLowerCase()] = value / propConversion[i];
-	}
-	return newInfo;
-}
-
-function onViewOpened(data){
-	eProps.remove();
-	// If this is the first time, create and load all properties
-	for (var i = 0; i < editableProps.length; i++){
-		var item = ePropBlueprint.clone().removeClass('blueprint');
-		ePropBlueprint.parent().append(item);
-		eProps = eProps.add(item);
-		item.find('.name').text(editableProps[i] + ' ('+ propUnits[i] +')');
-		var value = parseFloat(data.host.HostInfo[editableProps[i]]);
-		item.find('.value').text(util.round(value * propConversion[i]));
-	}
-}
-
-function update(data){
-	var total = util.formatBytes(data.host.HostInfo.TotalStorage);
-	var remaining = util.formatBytes(data.host.HostInfo.StorageRemaining);
-	var storage = util.formatBytes(data.host.HostInfo.TotalStorage - data.host.HostInfo.StorageRemaining);
-	var profit = data.host.HostInfo.Profit;
-	var potentialProfit = data.host.HostInfo.PotentialProfit;
-
-	eContracts.html(data.host.HostInfo.NumContracts + ' Contracts');
-	eStorage.html(storage + '/' + total + ' in use');
-	eRemaining.html(remaining + ' left')
-	eProfit.html((util.siacoin(profit)).toFixed(4) + ' KS earned');
-	ePotentialProfit.html((util.siacoin(potentialProfit)).toFixed(4) + ' KS to be earned');
-
-	var d = new Date();
-	if (updateTime < d.getTime() - 15000) {
-		document.getElementById('hmessage').innerHTML = 'Estimated Competitive Price: ' + 1000 * util.siacoin(data.host.HostInfo.Competition).toFixed(3) + ' SC / GB / Month';
-		updateTime = d.getTime();
-	}
-}
