9fd35a1a3bcf1b440f0d77d3bb5e634f5aeecf0e
Mingling94 <mcluo1994@gmail.com>
2015-08-26 09:51:57 -0400

    Sliding upload file frame

--- app/plugins/Files/js/buttons.js
+++ app/plugins/Files/js/buttons.js
@@ -3,13 +3,16 @@
 // TODO: Oust jquery, import in index.html, make it work
 eID('new-file').onclick = function(e) {
 	// TODO: Slide in add-file frame
-});
+	eID('add-file').classList.remove('closed');
+};
+/*
 eID('search-bar').keydown(function(e) {
 	// TODO: figure out search bar to work on enter key
 	updateFileList(lastLoadedFiles);
-});
+};
 eID('search').onclick = function(e) {
 	// TODO: figure out search bar to work on enter key
 	updateFileList(lastLoadedFiles);
-});
+};
+*/
 // TODO: Add button responses for add-file frame
