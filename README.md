cf183887aa8c2f3fe938217a3eba898c25238005
lukechampine <luke.champine@gmail.com>
2015-10-25 22:54:49 -0400

    bump version to v0.4.8-beta

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sia UI - 0.4.7-beta
+# Sia UI - 0.4.8-beta
 
 This is the user interface for [Sia](https://github.com/NebulousLabs/Sia), it
 is a desktop application based off the
--- release.sh
+++ release.sh
@@ -7,8 +7,8 @@ readonly RELEASE_DIR="release"
 readonly ELECTRON_URL="https://github.com/atom/electron/releases/download"
 readonly ELECTRON_VERSION="v0.33.9"
 readonly SIA_UI_NAME="Sia-UI"
-readonly SIA_VERSION="v0.4.5-beta"
-readonly SIA_UI_VERSION="v0.4.7-beta"
+readonly SIA_VERSION="v0.4.6-beta"
+readonly SIA_UI_VERSION="v0.4.8-beta"
 readonly SIA_RELEASE_DIR="$GOPATH/src/github.com/NebulousLabs/Sia/release/$SIA_VERSION"
 
 # generic packaging function
