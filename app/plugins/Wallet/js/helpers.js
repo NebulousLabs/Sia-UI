f1d2a6770ff564f978fa45ad9fa576b96ae3de04
Ming Luo <mcluo1994@gmail.com>
2015-10-26 17:45:00 -0400

    Nodified the UI
    Took out the minified bignumber and jquery and added them as
    dependencies
    
    Conflicts:
    	app/plugins/Files/js/helpers.js

--- app/plugins/Wallet/js/helpers.js
+++ app/plugins/Wallet/js/helpers.js
@@ -4,7 +4,7 @@
 // Library for communicating with Sia-UI
 const IPC = require('ipc');
 // Library for arbitrary precision in numbers
-const BigNumber = require('../../js/bignumber.min.js');
+const BigNumber = require('bignumber.js');
 // Ensure precision
 BigNumber.config({ DECIMAL_PLACES: 30 });
 BigNumber.config({ EXPONENTIAL_AT: 1e+9 });
