3f711b9f75582d976bc431cefee5e974f7294b64
Ming Luo <mcluo1994@gmail.com>
2015-11-03 16:58:00 -0500

    siac is imported & archives are made

--- .cleanRelease.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit # Exit on error
-
-rm -rf release/Sia-UI-linux-x64/resources/default_app
-rm -rf release/Sia-UI-linux-ia32/resources/default_app
-rm -rf release/Sia-UI-win32-x64/resources/default_app
-rm -rf release/Sia-UI-win32-ia32/resources/default_app
-rm -rf release/Sia-UI-darwin-x64/Sia-UI.app/Contents/Resources/default_app
