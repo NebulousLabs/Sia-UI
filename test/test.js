cdd26e740cdf6707fcd8dc1cf1b171125d5a6f56
Ming Luo <mcluo1994@gmail.com>
2015-11-10 17:51:12 -0500

    documented

--- test/test.js
+++ test/test.js
@@ -1,33 +1,47 @@
-var Application = require('spectron').Application;
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var path = require('path');
+'use strict';
 
-chai.should();
-chai.use(chaiAsPromised);
+// Libraries required for testing
+const Application = require('spectron').Application;
+const Chai = require('chai');
+const ChaiAsPromised = require('chai-as-promised');
+const Path = require('path');
 
+// Chai's should syntax is executed to edit Object to have Object.should
+Chai.should();
+// Chai's should syntax is extended to deal well with Promises
+Chai.use(ChaiAsPromised);
+
+// The one app that this suite is testing is Sia-UI
 describe('Sia-UI', function () {
 	var app;
 	var client;
+
+	// Starts a new session and assigns spectron's Application instance to a
+	// variable, app, available to all tests
 	beforeEach(function () {
 		app = new Application({
-			path: path.join(__dirname, '../node_modules/.bin/electron'),
-			args: ['--app=' + path.join(__dirname, '..')],
+			Path: Path.join(__dirname, '../node_modules/.bin/electron'),
+			args: ['--app=' + Path.join(__dirname, '..')],
 		});
 		return app.start();
 	});
 
+	// Extends ChaiAsPromised's syntax with spectron's electron-specific
+	// functions and assigns spectron's WebDriverIO properties to a variable,
+	// client, available to all tests
 	beforeEach(function () {
 		client = app.client;
-		chaiAsPromised.transferPromiseness = client.transferPromiseness;
+		ChaiAsPromised.transferPromiseness = client.transferPromiseness;
 	});
 
+	// Close Sia-UI session after each test-suite
 	afterEach(function () {
 		if (app && app.isRunning()) {
 			return app.stop();
 		}
 	});
 
+	// Test basic startup properties
 	describe('on startup', function () {
 		it('opens Sia-UI properly', function () {
 			return client.waitUntilWindowLoaded()
