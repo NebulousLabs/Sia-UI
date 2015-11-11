467a668338e6cf68b693b200ef8cfa2cdc283bb4
Ming Luo <mcluo1994@gmail.com>
2015-11-11 16:26:08 -0500

    Broked up basic tests for better error messages

--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
 'use strict';
 
 // Libraries required for testing
-const Application = require('spectron').Application;
-const Chai = require('chai');
-const ChaiAsPromised = require('chai-as-promised');
-const Path = require('path');
-const Fs = require('fs');
+var Application = require('spectron').Application;
+var Chai = require('chai');
+var ChaiAsPromised = require('chai-as-promised');
+var Path = require('path');
 
 // Chai's should syntax is executed to edit Object to have Object.should
 var should = Chai.should();
@@ -13,16 +12,16 @@ var should = Chai.should();
 Chai.use(ChaiAsPromised);
 
 // The one app that this suite is testing is Sia-UI
-describe('Sia-UI', function () {
+describe('main process', function() {
 	var app;
 	var client;
 
 	// Starts a new session and assigns spectron's Application instance to a
 	// variable, app, available to all tests
-	beforeEach(function () {
+	before('start electron', function() {
 		app = new Application({
 			path: Path.join(__dirname, '../node_modules/.bin/electron'),
-			args: ['--app=' + Path.join(__dirname, '..')],
+			args: [Path.join(__dirname, '..')],
 		});
 		return app.start();
 	});
@@ -30,40 +29,38 @@ describe('Sia-UI', function () {
 	// Extends ChaiAsPromised's syntax with spectron's electron-specific
 	// functions and assigns spectron's WebDriverIO properties to a variable,
 	// client, available to all tests
-	beforeEach(function () {
+	before('transfer spectron methods', function() {
 		client = app.client;
 		ChaiAsPromised.transferPromiseness = client.transferPromiseness;
 	});
 
-	// Close Sia-UI session after each test-suite
-	afterEach(function () {
+	// Close session after each test-suite
+	after('stop electron', function() {
 		if (app && app.isRunning()) {
 			return app.stop();
 		}
 	});
 
 	// Test basic startup properties
-	describe('on startup', function () {
-		it('opens Sia-UI properly', function () {
-			return client.waitUntilWindowLoaded()
-				.getWindowCount().then(function(count) {
-					Fs.readdir(Path.join(__dirname, '../plugins'), function(err, plugins) {
-						should.not.exist(err);
-						// Not sure why, but the plugins sometimes register as
-						// part of the Window Count
-						count.should.equal(1 || 1 + plugins.length);
-					});
-				})
-				.isWindowMinimized().should.eventually.be.false
-				.isWindowDevToolsOpened().should.eventually.be.false
-				.isWindowVisible().should.eventually.be.true
-				.isWindowFocused().should.eventually.be.true
-				.getWindowWidth().should.eventually.be.above(0)
-				.getWindowHeight().should.eventually.be.above(0)
-				.getArgv().then(function(argv) {
-					argv[0].should.contain('electron');
-					argv[1].should.contain('Sia-UI');
-				});
+	describe('on startup', function() {
+		it('opens a window', function() {
+			return client.getWindowCount().should.eventually.equal(1);
+		});
+		it('isn\'t minimized', function() {
+			return client.isWindowMinimized().should.eventually.be.false;
+		});
+		it('has devtools line commented out', function() {
+			return client.isWindowDevToolsOpened().should.eventually.be.false;
+		});
+		it('is visible', function() {
+			return client.isWindowVisible().should.eventually.be.true;
+		});
+		it('is in focus', function() {
+			return client.isWindowFocused().should.eventually.be.true;
+		});
+		it('has non-zero width and height', function() {
+			return client.getWindowWidth().should.eventually.be.above(0)
+				.getWindowHeight().should.eventually.be.above(0);
 		});
 	});
 });
