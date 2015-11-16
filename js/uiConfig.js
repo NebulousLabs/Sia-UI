09fbc49b7dea467c0fcf560908ef76205182fb55
Ming Luo <mcluo1994@gmail.com>
2015-11-16 13:43:46 -0500

    Can load json files with require

--- js/uiConfig.js
+++ js/uiConfig.js
@@ -49,15 +49,11 @@ module.exports = {
 	 * @param {callback} callback
 	 */
 	load: function(path, callback) {
-		Fs.readFile(path, function(err, data) {
-			if (err || data === 'undefined') {
-				// no file found, use default config
-				callback(defaultConfig);
-			} else {
-				// found config, use it
-				callback(JSON.parse(data));
-			}
-		});
+		try {
+			callback(require(path));
+		} catch (err) {
+			callback(defaultConfig);
+		}
 	},
 
 	/**
