c918799fb9b4ae45e004753817a647fdc916af22
Ming Luo <mcluo1994@gmail.com>
2015-12-09 10:52:14 -0500

    Use sia.js for About plugin

--- plugins/About/js/about.js
+++ plugins/About/js/about.js
@@ -2,24 +2,23 @@
 
 // Library for communicating with Sia-UI
 const IPCRenderer = require('electron').ipcRenderer;
+// Siad wrapper
+const Siad = require('sia.js');
 // Keeps track of if the view is shown
 var updating;
 
 // Update version every 50 seconds that this plugin is open
 function update() {
-	IPCRenderer.sendToHost('api-call', '/daemon/version', 'version');
-	
+	Siad.call('/daemon/version', function(err, result) {
+		if (err) {
+			IPCRenderer.sendToHost('notify', '/daemon/version call failed!', 'error');
+		} else {
+			document.getElementById('siaversion').innerHTML = result;
+		}
+	});
+
 	updating = setTimeout(update, 50000);
 }
-
-// Receive version
-IPCRenderer.on('version', function(event, err, result) {
-	if (err) {
-		IPCRenderer.sendToHost('notify', '/daemon/version call failed!', 'error');
-	}
-
-	document.getElementById('siaversion').innerHTML = result;
-});
 
 // Called by the UI upon showing
 function start() {
