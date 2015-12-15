433d12d54d269f85e7b4142e5ff1cd6ace990092
Ming Luo <mcluo1994@gmail.com>
2015-12-15 11:34:07 -0500

    `npm install` downloads Sia on postinstall script
    This, with the previous commit, means a golang setup is no longer a
    prerequisite to running the UI

---
+++ js/justGetSia.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// This script, much like how the name says, downloads a Sia release into the
+// UI's root directory.
+const SiadWrapper = require('sia.js');
+const Path = require('path')
+
+SiadWrapper.download(Path.join(__dirname, '..', 'Sia'));
