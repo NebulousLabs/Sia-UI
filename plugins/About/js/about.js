8df1ae2255a20b83c2727907cdbf737c7c33eb44
Ming Luo <mcluo1994@gmail.com>
2016-01-13 12:15:45 -0500

    FAQ button in About plugin

--- plugins/About/js/about.js
+++ plugins/About/js/about.js
@@ -1,19 +1,24 @@
 'use strict';
 
 // Library for communicating with Sia-UI
-const IPCRenderer = require('electron').ipcRenderer;
+const electron = require('electron');
+const ipcRenderer = require('electron').ipcRenderer;
 // Siad wrapper
 const Siad = require('sia.js');
 
 // Make sure Siad settings are in sync with the rest of the UI's
-var settings = IPCRenderer.sendSync('config', 'siad');
+var settings = ipcRenderer.sendSync('config', 'siad');
 Siad.configure(settings);
 
 // Update version shown
 Siad.call('/daemon/version', function(err, result) {
 	if (err) {
-		IPCRenderer.sendToHost('notification', err.toString(), 'error');
+		ipcRenderer.sendToHost('notification', err.toString(), 'error');
 	} else {
 		document.getElementById('siaversion').innerHTML = result.version;
 	}
 });
+
+document.getElementById('faq').onclick = function() {
+	electron.shell.openExternal('http://sia.tech/faq');
+}
