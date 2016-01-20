80df8e7cc7f4bc21936c9667c67103fb215c5708
lukechampine <luke.champine@gmail.com>
2016-01-20 17:30:09 -0500

    append a / to folder names

--- plugins/Files/js/folderElement.js
+++ plugins/Files/js/folderElement.js
@@ -30,6 +30,8 @@ function updateFolderElement(f, el) {
 function makeFolderElement(f, navigateTo) {
 	var el = fileElement(f);
 	el.addClass('folder');
+	// add a / to distinguish folders
+	el.find('.name').text(f.name + '/');
 
 	// Populate its fields and graphics
 	updateFolderElement(f, el);
