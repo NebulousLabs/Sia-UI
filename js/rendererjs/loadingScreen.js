adefaba9c402e940f7636a3da0ad13ed4ab8e86d
Johnathan Howell <me@johnathanhowell.com>
2016-05-13 04:49:06 -0400

    implement non-jquery fadeout animation

--- js/rendererjs/loadingScreen.js
+++ js/rendererjs/loadingScreen.js
@@ -1,29 +1,45 @@
 'use strict';
 
-// Node modules
+// loadingScreen.js: display a loading screen and launch the Siad API
+// if an available daemon is not running on the host,
+// launch an instance of siad using config.js.
+
 const Electron = require('electron');
 const IPCRenderer = Electron.ipcRenderer;
 const Siad = require('sia.js');
 const config = require('config.js');
-
-// Hide the UI with an overlay to ensure siad is running first
 const overlay = document.getElementsByClassName('overlay centered');
 
-// Export function 
-module.exports = function(initUI) {
+overlay.fadeOut = function(fadetime, interval, elapsed) {
+	overlay.style.opacity = elapsed / fadetime;
+	elapsed += interval;
 
-	// Start the Sia UI and display a welcome message
+	if (elapsed <== fadetime) {
+		setTimeout(fadetime, interval, elapsed);
+	} else {
+		overlay.style.visibility = "hidden";
+	}
+}
+
+module.exports = function(initUI) {
+	// These constants determine the behaviour of the overlay's fadeout animation.
+	const fadetime = 300; // 300 ms
+	const interval = 30; // 30 ms
+
+	// Check if Siad is already running on this host.
+	// If it is, start the UI and display a welcome message to the user.
+	// Otherwise, start a new instance of Siad using config.js.
+	Siad.ifRunning(startUI('Welcome back!'), startSiad());
+
 	const startUI = function(welcomemsg) {
-		const config = require('config.js');
+		// Initialize the Sia UI and display a welcome message
 		initUI();
 		document.getElementsByClassName('overlay centered').textContent = welcomemsg;
-	};
 
+		// Fade out the welcome message over a period of 300ms, updating the opacity every 20 ms.
+		overlay.fadeOut(fadetime, interval);
+	};
 
 	const startSiad = function() {
-		//TODO...
 	};
-	// Check if Siad is already running and funtional on this host
-	// If it is, start the UI and display a welcome message to the user
-	Siad.ifRunning(startUI('Welcome back!'), startSiad());
 };
