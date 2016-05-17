f0c3fa418c32af2f5202f642647e03376b223308
Johnathan Howell <me@johnathanhowell.com>
2016-05-16 21:44:35 -0400

    add config.path validation check and siad locator dialog

--- js/rendererjs/loadingScreen.js
+++ js/rendererjs/loadingScreen.js
@@ -5,9 +5,12 @@
 // launch an instance of siad using config.js.
 
 const remote = require('electron').remote;
-const IPCRenderer = require('electron').ipcRenderer;
+const dialog = remote.dialog;
+const fs = remote.require('fs');
+const Path = require('path');
+const IPC = require('electron').ipcRenderer;
 const Siad = require('sia.js');
-const config = IPCRenderer.sendSync('config', 'siad');
+const config = IPC.sendSync('config', 'siad');
 Siad.configure(config);
 
 const overlay = document.getElementsByClassName('overlay')[0];
@@ -18,6 +21,17 @@ const showError = (error) => {
 	overlayText.textContent = 'A Sia-UI error has occured: ' + error;
 };
 
+// checkSiaPath validates config's Sia path.
+// returns a promise that is resolved if the path is a valid directory
+const checkSiaPath = () => new Promise((resolve, reject) => {
+	fs.stat(config.path, (err, stats) => {
+		if (!err && stats.isDirectory()) {
+			resolve();
+		} else {
+			reject();
+		}
+	});
+});
 // startUI starts a Sia UI instance using the given welcome message.
 // calls initUI() after displaying a welcome message.
 const startUI = (welcomemsg, initUI) => {
@@ -30,6 +44,8 @@ const startUI = (welcomemsg, initUI) => {
 // startSiad configures and starts a Siad instance.
 // callback is called on successful start.
 const startSiad = (callback) => {
+	config.detached = false;
+	IPC.sendSync('config', 'siad', config);
 	Siad.configure(config, (error) => {
 		if (error) {
 			console.error(error);
@@ -44,17 +60,34 @@ const startSiad = (callback) => {
 // If it is, start the UI and display a welcome message to the user.
 // Otherwise, start a new instance of Siad using config.js.
 module.exports = function(initUI) {
-	Siad.ifRunning(() => {
-		config.detached = true;
-		IPCRenderer.sendSync('config', 'siad', config);
-		Siad.configure(config);
-		startUI('Welcome back', initUI);
-	}, () => {
-		config.detached = false;
-		IPCRenderer.sendSync('config', 'siad', config);
+	checkSiaPath().then(() => {
+		Siad.ifRunning(() => {
+			config.detached = true;
+			IPC.sendSync('config', 'siad', config);
+			Siad.configure(config);
+			startUI('Welcome back', initUI);
+		}, () => {
+			startSiad((error) => {
+				if (error) {
+					showError(error);
+				} else {
+					startUI('Welcome to Sia', initUI);
+				}
+			});
+		});
+	}).catch(() => {
+		// config.path doesn't exist.  Prompt the user for siad's location
+		dialog.showErrorBox('Siad not found', 'Sia-UI couldnt locate siad.  Please navigate to siad.');
+		const siadPath = dialog.showOpenDialog({
+			title: 'Please locate siad.',
+			properties: ['openFile'],
+			defaultPath: Path.join('../', config.path),
+			filters: [{ name: 'siad', extensions: ['*'] }],
+		});
+		config.path = Path.join(siadPath[0], '../');
 		startSiad((error) => {
 			if (error) {
-				console.error(error);
+				showError(error);
 			} else {
 				startUI('Welcome to Sia', initUI);
 			}
