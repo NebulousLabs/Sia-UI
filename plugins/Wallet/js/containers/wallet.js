345c43848c323fee05318a2e2655458c3c645051
Johnathan Howell <me@johnathanhowell.com>
2016-05-20 14:18:53 -0400

    hook up balance state to wallet container

--- plugins/Wallet/js/containers/wallet.js
+++ plugins/Wallet/js/containers/wallet.js
@@ -2,7 +2,8 @@ import WalletView from '../components/wallet.js'l;
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => ({
-
+	confirmedbalance: state.wallet.get('confirmedbalance'),
+	unconfirmedbalance: state.wallet.get('unconfirmedbalance'),
 });
 
 const Wallet = connect(mapStateToProps)(WalletView);
