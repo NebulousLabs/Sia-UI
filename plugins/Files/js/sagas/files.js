d69e0256b9ea8193d5baaf2c24fb17ed0d78d6cb
Johnathan Howell <me@johnathanhowell.com>
2016-06-03 18:38:43 -0400

    implement allowance sagas

--- plugins/Files/js/sagas/files.js
+++ plugins/Files/js/sagas/files.js
@@ -40,9 +40,21 @@ function* getFilesSaga() {
 	}
 }
 
+function* getAllowanceSaga() {
+	try {
+		const allowance = yield siadCall('/renter/allowance')
+		yield put(actions.receiveAllowance(allowance))
+	} catch (e) {
+		sendError(e)
+	}
+}
+
 export function* watchGetWalletLockstate() {
 	yield *takeEvery(constants.GET_WALLET_LOCKSTATE, getWalletLockstateSaga)
 }
 export function* watchGetFiles() {
 	yield *takeEvery(constants.GET_FILES, getFilesSaga)
+}
+export function* watchGetAllowance() {
+	yield *takeEvery(constants.GET_ALLOWANCE, getAllowanceSaga)
 }
