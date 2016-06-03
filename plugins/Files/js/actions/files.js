98220d835dfee9ea0f379f7ff094235c14a28769
Johnathan Howell <me@johnathanhowell.com>
2016-06-03 18:33:55 -0400

    add allowance actions

--- plugins/Files/js/actions/files.js
+++ plugins/Files/js/actions/files.js
@@ -14,3 +14,14 @@ export const setFiles = (files) => ({
 	type: constants.SET_FILES,
 	files,
 })
+export const getAllowance = () => ({
+	type: constants.GET_ALLOWANCE,
+})
+export const receiveAllowance = (allowance) => ({
+	type: constants.RECEIVE_ALLOWANCE,
+	allowance,
+})
+export const setAllowance = (allowance) => ({
+	type: constants.SET_ALLOWANCE,
+	allowance,
+})
