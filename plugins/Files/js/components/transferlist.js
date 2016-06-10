99e5dbece278cc72efa56f657e014dda6edc6db3
Johnathan Howell <me@johnathanhowell.com>
2016-06-10 16:41:11 -0400

    add transfer list component

---
+++ plugins/Files/js/components/transferlist.js
@@ -0,0 +1,25 @@
+import React, { PropTypes } from 'react'
+import { List } from 'immutable'
+
+const TransferList = ({transfers}) => {
+	const fileTransfers = transfers.map((transfer, key) => (
+		<li key={key}>
+			<div className="completed">{transfer.completed}</div>
+			<div className="filename">{transfer.name}</div>
+			<div className="progress">{transfer.progress}</div>
+		</li>
+	))
+	return (
+		<div className="transfer-list">
+			<ul>
+				{fileTransfers}
+			</ul>
+		</div>
+	)
+}
+
+TransferList.propTypes = {
+	transfers: PropTypes.instanceOf(List).isRequired,
+}
+
+export default TransferList
