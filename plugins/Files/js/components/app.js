09a81a3f04ba0487e663966f25f69091b30ac54c
Johnathan Howell <me@johnathanhowell.com>
2016-06-21 12:05:51 -0400

    change unlocked branch to ternary, improve styling

--- plugins/Files/js/components/app.js
+++ plugins/Files/js/components/app.js
@@ -2,26 +2,12 @@ import React, { PropTypes } from 'react'
 import FileBrowser from '../containers/filebrowser.js'
 import AllowanceDialog from '../containers/allowancedialog.js'
 
-const FilesApp = ({unlocked, showAllowanceDialog}) => {
-	let fileBrowserContent
-	if (!unlocked) {
-		fileBrowserContent = (
-			<div className="unlock-dialog">
-				You must unlock your wallet you can upload files!
-			</div>
-		)
-	} else {
-		fileBrowserContent = (
-			<FileBrowser />
-		)
-	}
-	return (
-		<div className="app">
-			{showAllowanceDialog ? <AllowanceDialog /> : null}
-			{fileBrowserContent}
-		</div>
-	)
-}
+const FilesApp = ({unlocked, showAllowanceDialog}) => (
+	<div className="app">
+		{showAllowanceDialog ? <AllowanceDialog /> : null}
+		{unlocked ? <FileBrowser /> : <div className="unlock-dialog"><h3>You must unlock your wallet you can upload files!</h3></div>}
+	</div>
+)
 
 FilesApp.propTypes = {
 	unlocked: PropTypes.bool.isRequired,
