a3dd72374fc3c000e5564ff66d8475760957e580
Johnathan Howell <me@johnathanhowell.com>
2016-06-22 16:26:16 -0400

    add Wallet Locked header to password prompt

--- plugins/Wallet/js/components/passwordprompt.js
+++ plugins/Wallet/js/components/passwordprompt.js
@@ -5,6 +5,7 @@ const PasswordPrompt = ({password, error, actions}) => {
 	const onUnlockClick = () => actions.unlockWallet(password)
 	return (
 		<div className="password-prompt">
+			<h2> Wallet Locked </h2>
 			<span> Enter your wallet password to unlock the wallet. </span>
 			<i className="fa fa-lock fa-4x"></i>
 			<input type="password" value={password} className="password-input" onChange={onPasswordChange} />
