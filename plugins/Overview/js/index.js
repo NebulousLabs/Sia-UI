e8950f020cde21ac01d5e0a321e3eb0a10329595
Johnathan Howell <me@johnathanhowell.com>
2016-06-22 02:04:41 -0400

    lint the overview plugin

--- plugins/Overview/js/index.js
+++ plugins/Overview/js/index.js
@@ -1,14 +1,11 @@
-'use strict';
+'use strict'
 
 // Library for arbitrary precision in numbers
-const BigNumber = require('bignumber.js');
+const BigNumber = require('bignumber.js')
 
 // Ensure precision
-BigNumber.config({ DECIMAL_PLACES: 24 });
-BigNumber.config({ EXPONENTIAL_AT: 1e+9 });
-
-// Keeps track of if the view is shown
-var updating;
+BigNumber.config({ DECIMAL_PLACES: 24 })
+BigNumber.config({ EXPONENTIAL_AT: 1e+9 })
 
 // DEVTOOL: uncomment to bring up devtools on plugin view
 // IPCRenderer.sendToHost('devtools');
@@ -17,61 +14,61 @@ var updating;
 function errored(err, result) {
 	if (err) {
 		SiaAPI.showError({title: 'Error', content: err.toString()})
-		return true;
+		return true
 	} else if (!result) {
 		SiaAPI.showError({title: 'Error', content: err.toString()})
-		return true;
+		return true
 	}
-	return false;
+	return false
 }
 
 // Convert to Siacoin
 function formatSiacoin(hastings) {
-	var number = new BigNumber(hastings);
-	var ConversionFactor = new BigNumber(10).pow(24);
+	var number = new BigNumber(hastings)
+	var ConversionFactor = new BigNumber(10).pow(24)
 	// Display two digits of Siacoin
-	var display = number.dividedBy(ConversionFactor).round(2) + ' S';
-	return display;
+	var display = number.dividedBy(ConversionFactor).round(2) + ' S'
+	return display
 }
 
 // Update wallet balance and lock status from call result
 function updateWallet(err, result) {
 	if (errored(err, result)) {
-		return;
+		return
 	}
 
-	var unlocked = result.unlocked;
-	var unencrypted = !result.encrypted;
+	var unlocked = result.unlocked
+	var unencrypted = !result.encrypted
 
-	var lockText = unencrypted ? 'New Wallet' : unlocked ? 'Unlocked' : 'Locked';
-	document.getElementById('lock').innerText = lockText;
+	var lockText = unencrypted ? 'New Wallet' : unlocked ? 'Unlocked' : 'Locked'
+	document.getElementById('lock').innerText = lockText
 
-	var bal = unlocked ? formatSiacoin(result.confirmedsiacoinbalance) : '--';
-	document.getElementById('balance').innerText = 'Balance: ' + bal;
+	var bal = unlocked ? formatSiacoin(result.confirmedsiacoinbalance) : '--'
+	document.getElementById('balance').innerText = 'Balance: ' + bal
 }
 
 // Update peer count from call result
 function updatePeers(err, result) {
 	if (errored(err, result)) {
-		return;
+		return
 	}
-	document.getElementById('peers').innerText = 'Peers: ' + result.peers.length;
+	document.getElementById('peers').innerText = 'Peers: ' + result.peers.length
 }
 
 // Update block height from call result
 function updateHeight(err, result) {
 	if (errored(err, result)) {
-		return;
+		return
 	}
-	document.getElementById('height').innerText = 'Block Height: ' + result.height;
+	document.getElementById('height').innerText = 'Block Height: ' + result.height
 }
 
 // Make API calls, sending a channel name to listen for responses
 function update() {
-	SiaAPI.call('/wallet', updateWallet);
-	SiaAPI.call('/gateway', updatePeers);
-	SiaAPI.call('/consensus', updateHeight);
-	updating = setTimeout(update, 5000);
+	SiaAPI.call('/wallet', updateWallet)
+	SiaAPI.call('/gateway', updatePeers)
+	SiaAPI.call('/consensus', updateHeight)
+	setTimeout(update, 5000)
 }
 
 update()
