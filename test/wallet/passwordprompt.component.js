98ddb30cf91e43c0f6ada8f4bdde43c0c2604a38
Johnathan Howell <me@johnathanhowell.com>
2016-06-28 19:15:06 -0400

    add password prompt rendering tests

--- test/wallet/passwordprompt.component.js
+++ test/wallet/passwordprompt.component.js
@@ -12,6 +12,15 @@ const testActions = {
 const passwordpromptComponent = shallow(<PasswordPrompt password="testpw" actions={testActions} />)
 
 describe('wallet password prompt component', () => {
+	it('renders a password input', () => {
+		expect(passwordpromptComponent.find('input .password-input')).to.have.length(1)
+		expect(passwordpromptComponent.find('input .password-input').first().prop('type')).to.equal('password')
+	})
+	it('renders a wallet unlock error', () => {
+		expect(passwordpromptComponent.find('.password-prompt-error').first().text()).to.equal('')
+		const erroredComponent = shallow(<PasswordPrompt password="testpw" error="testerror" />)
+		expect(erroredComponent.find('.password-prompt-error').first().text()).to.equal('testerror')
+	})
 	it('calls unlockWallet with password on unlock button click', () => {
 		passwordpromptComponent.find('.unlock-button').first().simulate('click')
 		expect(testActions.unlockWallet.calledWith('testpw')).to.be.true
