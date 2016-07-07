9cf831e3a6d80feb62d59fd8e3a097dc8f67f669
Johnathan Howell <me@johnathanhowell.com>
2016-07-07 14:06:29 -0400

    add main.js for initWallet

---
+++ plugins/Wallet/js/main.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import createSagaMiddleware from 'redux-saga'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/index.js'
+import rootSaga from './sagas/index.js'
+import { getLockStatus, getBalance, getTransactions } from './actions/wallet.js'
+import WalletApp from './components/app.js'
+
+export const initWallet = () => {
+	// Set up saga middleware system
+	const sagaMiddleware = createSagaMiddleware()
+	const store = createStore(
+		rootReducer,
+		applyMiddleware(sagaMiddleware)
+	)
+	sagaMiddleware.run(rootSaga)
+	// Get initial UI state
+	store.dispatch(getLockStatus())
+	store.dispatch(getBalance())
+	store.dispatch(getTransactions())
+
+	// Poll Siad for state changes.
+	setInterval(() => {
+		store.dispatch(getLockStatus())
+		store.dispatch(getBalance())
+		store.dispatch(getTransactions())
+	}, 5000)
+	return (
+		<Provider store={store}>
+			<WalletApp />
+		</Provider>
+	)
+}
+
