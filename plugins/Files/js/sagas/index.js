3f6b84df757188a77a9646b87f43af54e1a87101
Johnathan Howell <me@johnathanhowell.com>
2016-07-20 17:06:08 -0400

    use Object.values(...).map to ensure every saga is run by the root saga

--- plugins/Files/js/sagas/index.js
+++ plugins/Files/js/sagas/index.js
@@ -2,20 +2,6 @@ import * as sagas from './files.js'
 import { fork } from 'redux-saga/effects'
 
 export default function* rootSaga() {
-	yield [
-		fork(sagas.watchGetWalletLockstate),
-		fork(sagas.watchGetFiles),
-		fork(sagas.watchSetAllowance),
-		fork(sagas.watchGetWalletBalance),
-		fork(sagas.watchCalculateStorageCost),
-		fork(sagas.watchSetAllowanceProgress),
-		fork(sagas.watchUploadFile),
-		fork(sagas.watchDownloadFile),
-		fork(sagas.watchGetDownloads),
-		fork(sagas.watchGetUploads),
-		fork(sagas.watchDeleteFile),
-		fork(sagas.watchUploadFolder),
-		fork(sagas.watchGetStorageMetrics),
-		fork(sagas.watchGetContractCount),
-	]
+	const watchers = Object.values(sagas).map((saga) => fork(saga))
+	yield watchers
 }
