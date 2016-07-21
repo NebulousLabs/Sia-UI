8aba40793b529534bd324e94eedfc2fc11c2e933
Johnathan Howell <me@johnathanhowell.com>
2016-07-20 20:18:11 -0400

    remove unnecessary arrow function

--- plugins/Files/js/sagas/index.js
+++ plugins/Files/js/sagas/index.js
@@ -2,6 +2,6 @@ import * as sagas from './files.js'
 import { fork } from 'redux-saga/effects'
 
 export default function* rootSaga() {
-	const watchers = Object.values(sagas).map((saga) => fork(saga))
+	const watchers = Object.values(sagas).map(fork)
 	yield watchers
 }
--- plugins/Wallet/js/sagas/index.js
+++ plugins/Wallet/js/sagas/index.js
@@ -2,6 +2,6 @@ import * as sagas from './wallet.js'
 import { fork } from 'redux-saga/effects'
 
 export default function* rootSaga() {
-	const watchers = Object.values(sagas).map((saga) => fork(saga))
+	const watchers = Object.values(sagas).map(fork)
 	yield watchers
 }
