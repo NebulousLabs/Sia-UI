c58b539c5fa4ba3131e99428152554bb79f92102
Steven Wallace <steven@thewallaces.ca>
2016-08-19 19:24:57 -0700

    renamedialog only shows filename instead of full path

--- plugins/Files/js/components/renamedialog.js
+++ plugins/Files/js/components/renamedialog.js
@@ -1,20 +1,21 @@
 import React, { PropTypes } from 'react'
+import Path from 'path'
 
 const RenameDialog = ({siapath, actions}) => {
 	const onYesClick = (e) => {
 		e.preventDefault()
-		actions.renameFile(siapath, e.target.newsiapath.value)
+		actions.renameFile(siapath, Path.join(Path.dirname(siapath), e.target.newsiapath.value))
 	}
 	const onNoClick = () => actions.hideRenameDialog()
 	return (
 		<div className="modal">
 			<div className="rename-dialog">
 				<div className="rename-text">
-					Enter a new name for {siapath}:
+					Enter a new name for {Path.basename(siapath)}:
 				</div>
 				<form className="rename-form" onSubmit={onYesClick}>
 					<div className="rename-field">
-						<input type="text" name="newsiapath" required autoFocus defaultValue={siapath} />
+						<input type="text" name="newsiapath" required autoFocus defaultValue={Path.basename(siapath)} />
 					</div>
 					<div className="rename-buttons">
 						<button type="submit">Confirm</button>
