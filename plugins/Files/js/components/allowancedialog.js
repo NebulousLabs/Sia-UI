520a20bbf4234d4d2fec5a5ba2b0af89c963e300
Johnathan Howell <me@johnathanhowell.com>
2016-10-12 17:30:46 -0400

    update allowance dialog text, styling, remove some dead constants

--- plugins/Files/js/components/allowancedialog.js
+++ plugins/Files/js/components/allowancedialog.js
@@ -10,14 +10,13 @@ const AllowanceDialog = ({unlocked, allowance, actions}) => {
 	const dialogContents = (
 		<div className="allowance-dialog">
 			<h3> Buy storage on the Sia Decentralized Network</h3>
-			<p>Enter an amount of SC to allocate towards uploading, downloading, and storing data on the Sia network.</p>
+			<p>Enter an amount of SC to allocate towards uploading, downloading, and storing data on the Sia network. This amount represents the maximum amount of funds the contractor will spend over a 12 week period. After 12 weeks, the contractor will renew the contracts it forms. Any allocated funds that are not used will be refunded.</p>
 			<form className="allowance-form" onSubmit={onAcceptClick}>
 				<input type="number" name="allowance" defaultValue={allowance} required autoFocus className="allowance-amount" />SC
 				<div className="allowance-buttons">
 					<button type="submit" className="allowance-button-accept">Accept</button>
 					<button type="button" onClick={onCancelClick} className="allowance-button-cancel">Cancel</button>
 				</div>
-				<p className="allowance-warning">Any unused funds will be refunded.</p>
 			</form>
 		</div>
 	)
