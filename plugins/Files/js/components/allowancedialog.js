a9141340200369ae2f9215224e69cb40c4e67132
Johnathan Howell <me@johnathanhowell.com>
2016-10-17 05:22:01 -0400

    improve styling

--- plugins/Files/js/components/allowancedialog.js
+++ plugins/Files/js/components/allowancedialog.js
@@ -14,9 +14,11 @@ const AllowanceDialog = ({unlocked, feeEstimate, storageEstimate, actions}) => {
 	const dialogContents = (
 		<div className="allowance-dialog">
 			<h3> Buy storage on the Sia Decentralized Network</h3>
-			<p>You need to allocate funds to upload and download on Sia. Your allowance remains locked for 3 months. Unspent funds are then refunded*. You can increase your allowance at any time.</p>
-			<p>Your storage allowance automatically refills every 6 weeks. Your computer must be online with your wallet unlocked to complete the refill. If Sia fails to refill the allowance by the end of the lock-in period, your data may be lost.</p>
-			<p>*contract fees are non-refundable</p>
+			<div className="allowance-message">
+				<p>You need to allocate funds to upload and download on Sia. Your allowance remains locked for 3 months. Unspent funds are then refunded*. You can increase your allowance at any time.</p>
+				<p>Your storage allowance automatically refills every 6 weeks. Your computer must be online with your wallet unlocked to complete the refill. If Sia fails to refill the allowance by the end of the lock-in period, your data may be lost.</p>
+				<p className="footnote">*contract fees are non-refundable</p>
+			</div>
 			<form className="allowance-form" onSubmit={onAcceptClick}>
 				<input type="number" name="allowance" defaultValue="5000" onFocus={onAllowanceChange} onChange={onAllowanceChange} required autoFocus className="allowance-amount" />SC
 				<div className="allowance-buttons">
