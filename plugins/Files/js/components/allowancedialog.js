3959554e76658c8624ceeb11dbf60bfa9239f296
Johnathan Howell <me@johnathanhowell.com>
2016-10-17 18:33:37 -0400

    clarify contract fees, make estimations gofmt-style

--- plugins/Files/js/components/allowancedialog.js
+++ plugins/Files/js/components/allowancedialog.js
@@ -17,10 +17,10 @@ const AllowanceDialog = ({unlocked, feeEstimate, storageEstimate, actions}) => {
 			<div className="allowance-message">
 				<p>You need to allocate funds to upload and download on Sia. Your allowance remains locked for 3 months. Unspent funds are then refunded*. You can increase your allowance at any time.</p>
 				<p>Your storage allowance automatically refills every 6 weeks. Your computer must be online with your wallet unlocked to complete the refill. If Sia fails to refill the allowance by the end of the lock-in period, your data may be lost.</p>
-				<p className="footnote">*contract fees are non-refundable</p>
+				<p className="footnote">*contract fees are non-refundable. They will be subtracted from the allowance that you set.</p>
 			</div>
 			<form className="allowance-form" onSubmit={onAcceptClick}>
-				<input type="number" name="allowance" defaultValue="5000" onFocus={onAllowanceChange} onChange={onAllowanceChange} required autoFocus className="allowance-amount" />SC
+				<input type="number" name="allowance" defaultValue="5000" onFocus={onAllowanceChange} onChange={onAllowanceChange} required autoFocus className="allowance-amount" /> SC
 				<div className="allowance-buttons">
 					<button type="submit" className="allowance-button-accept">Accept</button>
 					<button type="button" onClick={onCancelClick} className="allowance-button-cancel">Cancel</button>
@@ -28,12 +28,12 @@ const AllowanceDialog = ({unlocked, feeEstimate, storageEstimate, actions}) => {
 			</form>
 			<table className="estimates">
 				<tr>
-					<th>Estimated Fees</th>
-					<th>Estimated Storage</th>
+					<td className="estimate-label">Estimated Fees</td>
+					<td className="estimate-content">{feeEstimate} SC</td>
 				</tr>
 				<tr>
-					<td>{feeEstimate} SC</td>
-					<td>{storageEstimate}</td>
+					<td className="estimate-label">Estimated Storage</td>
+					<td className="estimate-content">{storageEstimate}</td>
 				</tr>
 			</table>
 		</div>
