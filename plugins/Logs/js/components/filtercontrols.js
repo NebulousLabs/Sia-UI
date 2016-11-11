a66600c4d8641068416ddeb49a25f79f678f65bc
Johnathan Howell <me@johnathanhowell.com>
2016-11-11 15:31:23 -0500

    move filterButtons to ./filters.js

--- plugins/Logs/js/components/filtercontrols.js
+++ plugins/Logs/js/components/filtercontrols.js
@@ -1,33 +1,7 @@
 import React, { PropTypes } from 'react'
 import { Set } from 'immutable'
 import FilterControl from './filtercontrol'
-
-const filterButtons = [
-	{
-		name: 'Consensus',
-		filters: ['consensus.log'],
-	},
-	{
-		name: 'Gateway',
-		filters: ['gateway.log'],
-	},
-	{
-		name: 'Host',
-		filters: ['host.log', 'storagemanager.log'],
-	},
-	{
-		name: 'Renter',
-		filters: ['renter/contractor.log', 'renter/hostdb.log', 'renter.log'],
-	},
-	{
-		name: 'Miner',
-		filters: ['miner.log'],
-	},
-	{
-		name: 'Wallet',
-		filters: ['wallet.log'],
-	},
-]
+import { filters } from '../filters.js'
 
 const filterControlsStyle = {
 	width: '100%',
@@ -44,7 +18,7 @@ const filterControlsStyle = {
 const FilterControls = ({logFilters, actions}) => (
 	<div style={filterControlsStyle}>
 		{
-			filterButtons.map((filter, key) => (
+			filters.map((filter, key) => (
 				<FilterControl
 					name={filter.name}
 					filters={filter.filters}
---
+++ plugins/Logs/js/filters.js
@@ -0,0 +1,27 @@
+export const filters = [
+	{
+		name: 'Consensus',
+		filters: ['consensus.log'],
+	},
+	{
+		name: 'Gateway',
+		filters: ['gateway.log'],
+	},
+	{
+		name: 'Host',
+		filters: ['host.log', 'storagemanager.log'],
+	},
+	{
+		name: 'Renter',
+		filters: ['renter/contractor.log', 'renter/hostdb.log', 'renter.log'],
+	},
+	{
+		name: 'Miner',
+		filters: ['miner.log'],
+	},
+	{
+		name: 'Wallet',
+		filters: ['wallet.log'],
+	},
+]
+
