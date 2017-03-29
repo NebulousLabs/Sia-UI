455924b94ab5429ae5373ca4e289446601f9c2fd
Johnathan Howell <me@johnathanhowell.com>
2017-03-29 18:02:34 -0400

    add helpful infobox on hover to the host status component

--- plugins/Hosting/js/components/hoststatus.js
+++ plugins/Hosting/js/components/hoststatus.js
@@ -6,6 +6,9 @@ const HostStatus = ({connectable, working}) => {
 			<div className="host-status">
 				<i className="fa fa-times offline-icon" />
 				<span> Host Unreachable </span>
+				<div className="host-status-info">
+					Your host is not connectable at the configured net address. Check your UPNP or NAT settings.
+				</div>
 			</div>
 		)
 	}
@@ -13,8 +16,11 @@ const HostStatus = ({connectable, working}) => {
 	if (connectable && !working) {
 		return (
 			<div className="host-status">
-				<i className="fa fa-times offline-icon" />
-				<span> Host Not Working </span>
+				<i className="fa fa-times inactive-icon" />
+				<span> Host Inactive </span>
+				<div className="host-status-info">
+					Your host is connectable, but it is not being used by any renters.
+				</div>
 			</div>
 		)
 	}
@@ -23,6 +29,9 @@ const HostStatus = ({connectable, working}) => {
 		<div className="host-status">
 			<i className="fa fa-check online-icon" />
 			<span> Host Online </span>
+			<div className="host-status-info">
+				Your host is connectable and is being contacted by renters.
+			</div>
 		</div>
 	)
 }
