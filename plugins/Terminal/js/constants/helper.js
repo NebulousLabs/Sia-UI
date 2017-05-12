6cf7718181643c8169dc2bba20dc7843591b74bc
Johnathan Howell <me@johnathanhowell.com>
2017-05-12 18:07:56 -0400

    add terminal support for init seed

--- plugins/Terminal/js/constants/helper.js
+++ plugins/Terminal/js/constants/helper.js
@@ -1,14 +1,16 @@
 export const REGULAR_COMMAND = -1
 export const WALLET_SEED = 0
-export const WALLET_UNLOCK = 1
-export const WALLET_033X = 2
-export const WALLET_SIAG = 3
-export const HELP = 4
-export const HELP_QMARK = 5
+export const WALLET_INIT_SEED = 1
+export const WALLET_UNLOCK = 2
+export const WALLET_033X = 3
+export const WALLET_SIAG = 4
+export const HELP = 5
+export const HELP_QMARK = 6
 
 //These commands need a password prompt or other special handling.
 export const specialCommands = [
 	['wallet', 'load', 'seed'],
+	['wallet', 'init-seed'],
 	['wallet', 'unlock'],
 	['wallet', 'load', '033x'],
 	['wallet', 'load', 'siag'],
