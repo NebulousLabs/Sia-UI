8dc47e4907bfb3cb3a3f5cc7f57dfb9b3218a472
Maël Lavault <mael.lavault@universalavenue.com>
2017-05-12 19:27:10 +0200

    Update to webpack2.
    Remove unused json-loader, fix eslint errors,
    move webpack and babel-loader in devDependencies.
    
    Signed-off-by: Maël Lavault <mael.lavault@universalavenue.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,31 @@
 const webpack = require('webpack')
 const path = require('path')
 const glob = require('glob')
+const version = require('./package.json').version
 
 // Compute the entry-points for Sia-UI.
-let entrypoints = {}
-const plugins = glob.sync("./plugins/*/js/index.js")
+const entrypoints = {}
+const plugins = glob.sync('./plugins/*/js/index.js')
 plugins.forEach((plugin) => {
 	entrypoints[path.dirname(path.dirname(plugin))] = ['babel-polyfill', path.resolve('./js/rendererjs/pluginapi.js'), plugin]
 })
 
-entrypoints["renderer"] = ['babel-polyfill', path.resolve('./js/rendererjs/index.js')]
-entrypoints["main"] = path.resolve('./js/mainjs/index.js')
+entrypoints['renderer'] = ['babel-polyfill', path.resolve('./js/rendererjs/index.js')]
+entrypoints['main'] = path.resolve('./js/mainjs/index.js')
 
 module.exports = {
 	entry: entrypoints,
 	plugins: [
 		new webpack.DefinePlugin({
-			VERSION: JSON.stringify(require("./package.json").version)
-		})
+			VERSION: JSON.stringify(version),
+		}),
 	],
 	output: {
-		path: path.resolve("./dist"),
-		filename: '[name].js'
+		path: path.resolve('./dist'),
+		filename: '[name].js',
 	},
 	resolve: {
-		root: path.resolve('./node_modules')
-	},
-	resolveLoader: {
-		root: path.resolve('./node_modules'),
+		modules: [path.resolve('./node_modules')],
 	},
 	node: {
 		__dirname: false,
@@ -40,16 +38,10 @@ module.exports = {
 		// this noParse is to deal with an issue with validate.js not being packed properly.
 		// see this issue: https://github.com/webpack/webpack/issues/138 for more information.
 		noParse: /node_modules\/json-schema\/lib\/validate\.js/,
-		preLoaders: [
-			{
-				test: /\.json$/,
-				loader: 'json-loader',
-			},
-		],
-		loaders: [
+		rules: [
 			{
 				test: /\.js?$/,
-				loader: 'babel',
+				use: 'babel-loader',
 				exclude: /node_modules/,
 			},
 		],
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,10 +4,9 @@ const webpack = require('webpack')
 
 const cfg = require('./webpack.config.js')
 cfg.plugins.push(new webpack.DefinePlugin({
-	"process.env": {
-		NODE_ENV: JSON.stringify("production")
-	}
+	'process.env': {
+		NODE_ENV: JSON.stringify('production'),
+	},
 }))
 
 module.exports = cfg
-
