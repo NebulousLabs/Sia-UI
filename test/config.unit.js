ba6513abe09efd7e69d007a01797272038a5b891
Johnathan Howell <me@johnathanhowell.com>
2017-06-22 12:56:56 -0400

    fix config.json upgrading issue

--- test/config.unit.js
+++ test/config.unit.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import fs from 'fs'
 import proxyquire from 'proxyquire'
+import { version } from '../package.json'
 
 const mock = {
 	'electron': {
@@ -28,6 +29,20 @@ describe('config.js', () => {
 		config.save()
 		const config2 = loadConfig('test.json')
 		expect(config2).to.deep.equal(config)
+	})
+	it('sets the default siad path if the config version is outdated', () => {
+		const config = loadConfig('test.json')
+		const defaultSiadPath = config.siad.path
+		config.version = '1.2.0'
+		config.siad.path = '/test/siad/path'
+		config.save()
+		const config2 = loadConfig('test.json')
+		expect(config2.siad.path).to.equal(defaultSiadPath)
+		expect(config2.version).to.equal(version)
+		config2.siad.path = '/test/new/path'
+		config2.save()
+		const config3 = loadConfig('test.json')
+		expect(config3.siad.path).to.equal('/test/new/path')
 	})
 })
 
