8bbffb3f558d60bc60136338fefca33b3d471679
Steven Wallace <steven@thewallaces.ca>
2017-07-10 16:40:43 -0700

    Added update check button

--- plugins/About/js/index.js
+++ plugins/About/js/index.js
@@ -1,4 +1,6 @@
 'use strict'
+const os = require('os')
+const util = require('util')
 
 // Set UI version via package.json.
 document.getElementById('uiversion').innerHTML = VERSION
@@ -13,3 +15,30 @@ SiaAPI.call('/daemon/version', (err, result) => {
 	}
 })
 
+function genDownloadLink(version, platform) {
+	let plat = platform
+	if (plat === 'darwin') {
+		plat = 'osx'
+	}
+
+	return util.format('https://github.com/NebulousLabs/Sia-UI/releases/download/v%s/Sia-UI-v%s-%s-x64.zip', version, version, plat)
+}
+
+function updateCheck() {
+	SiaAPI.call('/daemon/update', (err, result) => {
+		if (err) {
+			SiaAPI.showError('Error', err.toString())
+			ipcRenderer.sendToHost('notification', err.toString(), 'error')
+		} else if (result.available) {
+			document.getElementById('newversion').innerHTML = result.version
+			document.getElementById('downloadlink').href = genDownloadLink(result.version, os.platform())
+			document.getElementById('nonew').style.display = 'none'
+			document.getElementById('yesnew').style.display = 'block'
+		} else {
+			document.getElementById('nonew').style.display = 'block'
+			document.getElementById('yesnew').style.display = 'none'
+		}
+	})
+}
+
+document.getElementById('updatecheck').onclick = updateCheck
