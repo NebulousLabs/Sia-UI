6a529ac462660d47d49be5bfacd6127275bb13d6
Mark Huetsch <markhuetsch@gmail.com>
2017-11-06 17:00:53 +0800

    uploading has its own set of statuses, we need to do Downloading vs. everything else

--- plugins/Files/js/components/transfer.js
+++ plugins/Files/js/components/transfer.js
@@ -2,16 +2,18 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import ProgressBar from './progressbar.js'
 
-const Transfer = ({name, progress, status, speed, onClick}) => (
-	<li className="filetransfer" onClick={onClick}>
-		<div className="transfer-info">
-			<div className="transfername">{name}</div>
-			<ProgressBar progress={progress} />
-			{status === 'Downloading' && <span className="transfer-status">{status} - {speed}</span>}
-			{status === 'Completed' && <span className="transfer-status">{status}</span>}
-		</div>
-	</li>
-)
+const Transfer = ({name, progress, status, speed, onClick}) => {
+	const statusText = (status === 'Downloading') ? status + ' - ' + speed : status
+	return (
+		<li className="filetransfer" onClick={onClick}>
+			<div className="transfer-info">
+				<div className="transfername">{name}</div>
+				<ProgressBar progress={progress} />
+				<span className="transfer-status">{statusText}</span>
+			</div>
+		</li>
+	)
+}
 
 Transfer.propTypes = {
 	name: PropTypes.string.isRequired,
