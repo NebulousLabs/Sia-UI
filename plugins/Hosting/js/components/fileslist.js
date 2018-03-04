43f746a7733feb7d1d3b74b16ac1b09574806ffd
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-03-04 20:58:35 +0800

    Add styles for hosting

--- plugins/Hosting/js/components/fileslist.js
+++ plugins/Hosting/js/components/fileslist.js
@@ -19,20 +19,20 @@ const FilesList = ({ folders, folderPathToRemove, actions }) => {
 	const sortedFolders = folders.sortBy((folder) => folder.get('path'))
 
 	const FileList = sortedFolders.map((folder, key) => (
-		<div className="property pure-g" key={key}>
-			<div className="pure-u-3-4">
-				<div className="name">{folder.get('path')}</div>
+		<div className="property pure-g file-row" key={key}>
+			<div className="pure-u-1-2">
+				<div>{folder.get('path')}</div>
 			</div>
-			<div className="pure-u-1-12">
+			<div className="pure-u-1-8">
 				<div>{Math.floor(folder.get('free')).toString()} GB</div>
 			</div>
-			<div className="pure-u-1-12">
+			<div className="pure-u-1-8">
 				<div>{Math.floor(folder.get('size')).toString()} GB</div>
 			</div>
-			<div className="pure-u-1-24" onClick={onResizeStorageLocationClick(folder)}>
+			<div className="pure-u-1-8" onClick={onResizeStorageLocationClick(folder)}>
 				<div><i className="fa fa-edit button" /></div>
 			</div>
-			<div className="pure-u-1-24" onClick={onRemoveStorageLocationClick(folder)}>
+			<div className="pure-u-1-8" onClick={onRemoveStorageLocationClick(folder)}>
 				<div><i className="fa fa-remove button" /></div>
 			</div>
 			{
@@ -51,13 +51,14 @@ const FilesList = ({ folders, folderPathToRemove, actions }) => {
 			<div className="property row">
 				<div className="title" />
 				<div className="controls full">
-					<div className="button" id="edit" onClick={addStorageLocation}>
-						<i className="fa fa-folder-open" />
-						Add Storage Folder
+					<div className="pure-u-1-2">
+						<div className="button" id="edit" onClick={addStorageLocation}>
+							<i className="fa fa-folder-open" />
+							Add Storage Folder
+						</div>
 					</div>
-					<div className="pure-u-1-12" style={{ 'textAlign': 'left' }}>Free</div>
-					<div className="pure-u-1-12" style={{ 'textAlign': 'left' }}>Max</div>
-					<div className="pure-u-1-12" />
+					<div className="pure-u-1-8" style={{ 'textAlign': 'left' }}>Free</div>
+					<div className="pure-u-1-8" style={{ 'textAlign': 'left' }}>Max</div>
 				</div>
 			</div>
 			{FileList}
