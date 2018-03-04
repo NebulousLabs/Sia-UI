5273eb3ae2ef4447e336102bf6e7b133934689ae
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-03-05 03:54:35 +0800

    Add dialog styles

--- plugins/Wallet/js/components/backupprompt.js
+++ plugins/Wallet/js/components/backupprompt.js
@@ -5,24 +5,30 @@ const BackupPrompt = ({primarySeed, auxSeeds, actions}) => {
 	const handleOkClick = () => actions.hideBackupPrompt()
 	return (
 		<div className="modal">
-			<div className="backupprompt">
+			<div className="backupprompt dialog">
 				{ auxSeeds.length === 0 ? (
-					<h3> Write down your seed to back up your wallet. You can restore your wallet using only this seed. </h3>
+					<h3 className="dialog__title">
+						Write down your seed to back up your wallet. You can restore your wallet using only this seed.
+					</h3>
 				) : (
-					<h3> Write down your seeds to back up your wallet. You can restore your wallet using only these seeds. </h3>
+					<h3 className="dialog__title">
+						Write down your seeds to back up your wallet. You can restore your wallet using only these seeds.
+					</h3>
 				)}
 
-				<h4> Primary Seed: </h4>
-				<p className="primary-seed">{primarySeed}</p>
-				{auxSeeds.length > 0 ? ( <h4> Auxiliary Seeds: </h4> ) : null}
-				{auxSeeds.length > 0 ? (
-					<div className="aux-seeds">
-						{auxSeeds.map((seed, key) => (
-							<div className="aux-seed" key={key}>{seed}</div>
-						))}
-					</div>
-				) : null}
-				<button className="button ok-button" onClick={handleOkClick}>OK</button>
+				<div className="dialog__content">
+					<h4> Primary Seed: </h4>
+					<p className="primary-seed">{primarySeed}</p>
+					{auxSeeds.length > 0 ? ( <h4> Auxiliary Seeds: </h4> ) : null}
+					{auxSeeds.length > 0 ? (
+						<div className="aux-seeds">
+							{auxSeeds.map((seed, key) => (
+								<div className="aux-seed" key={key}>{seed}</div>
+							))}
+						</div>
+					) : null}
+					<button className="button ok-button" onClick={handleOkClick}>OK</button>
+				</div>
 			</div>
 		</div>
 	)
