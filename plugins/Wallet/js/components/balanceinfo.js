2d38397f92e57963ab81cf9b07b540039f312bb4
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-03-04 16:29:16 +0800

    Add styles for wallet balance

--- plugins/Wallet/js/components/balanceinfo.js
+++ plugins/Wallet/js/components/balanceinfo.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types'
 
 const BalanceInfo = ({synced, confirmedbalance, unconfirmedbalance, siafundbalance, siacoinclaimbalance}) => (
 	<div className="balance-info">
-		<span>Balance: {confirmedbalance} SC </span>
-		<span>Unconfirmed Delta: {unconfirmedbalance} SC </span>
-		{siafundbalance !== '0' ? (<span> Siafund Balance: {siafundbalance} SF </span>) : null}
-		{siacoinclaimbalance !== '0' ? (<span> Siacoin Claim Balance: {siacoinclaimbalance} SC </span>) : null}
+		<div>
+			<span className="balance-info__main-label">Available Balance</span>
+			<div>
+				<span className="balance-info__main-balance">{confirmedbalance}</span>
+				<span className="balance-info__main-balance-currency">SC</span>
+			</div>
+			{unconfirmedbalance !== '0' || true
+				&& <span className="balance-info__unconfirmed-balance">{unconfirmedbalance}<sup> SC</sup></span>
+			}
+		</div>
+		<div className="balance-info__addendum">
+			{siafundbalance !== '0' ? (<div> Siafund Balance: {siafundbalance} SF </div>) : null}
+			{siacoinclaimbalance !== '0' ? (<div> Siacoin Claim Balance: {siacoinclaimbalance} SC </div>) : null}
+		</div>
 		{!synced ? (
 			<span style={{marginRight: 40, color: 'rgb(255, 93, 93)', fontSize: 12}}><i className="fa fa-exclamation-triangle" />Your wallet is not synced, balances are not final.</span>
 		) : null
