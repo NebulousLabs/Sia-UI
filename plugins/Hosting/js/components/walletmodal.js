79ee7ce57e92fd2c69f59bf53ed23adc5e05b87d
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-03-08 02:41:09 +0800

    Standardize wallet modal

--- plugins/Hosting/js/components/walletmodal.js
+++ plugins/Hosting/js/components/walletmodal.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import Modal from './modal'
 
 const WalletUnlockModal = ({ walletLocked }) => (
-	<div className={'hosting-options-modal modal' + (walletLocked ? '': ' hidden')}>
-		<div className="hosting-options modal-message">
-			<h3>You must unlock the wallet to host files.</h3>
-			<i className="fa fa-lock fa-4x" />
+	<Modal open={walletLocked}>
+		<div className='hosting-options-modal dialog'>
+			<div className="hosting-options modal-message">
+				<h3 className="dialog__title">You must unlock the wallet to host files</h3>
+				<div className="dialog__content">
+					<i className="fa fa-lock fa-4x" />
+				</div>
+			</div>
 		</div>
-	</div>
+	</Modal>
 )
 
 export default WalletUnlockModal
