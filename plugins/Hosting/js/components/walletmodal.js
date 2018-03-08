e955d21ee9c18e1ba646f3110441041620c956d5
Arian Allenson M. Valdez <Secretmapper16@gmail.com>
2018-03-08 16:14:36 +0800

    [chore] Fix lint

--- plugins/Hosting/js/components/walletmodal.js
+++ plugins/Hosting/js/components/walletmodal.js
@@ -3,7 +3,7 @@ import Modal from './modal'
 
 const WalletUnlockModal = ({ walletLocked }) => (
 	<Modal open={walletLocked}>
-		<div className='hosting-options-modal dialog'>
+		<div className="hosting-options-modal dialog">
 			<div className="hosting-options modal-message">
 				<h3 className="dialog__title">You must unlock the wallet to host files</h3>
 				<div className="dialog__content">
