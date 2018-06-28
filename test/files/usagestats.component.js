f8574f2c9d836e23f1310c4041b10f90b1d41402
Eddie Wang <bonjour@eddiewang.me>
2018-06-28 14:23:34 -0400

    Fix tests for MacOS

--- test/files/usagestats.component.js
+++ test/files/usagestats.component.js
@@ -5,12 +5,12 @@ import UsageStats from '../../plugins/Files/js/components/usagestats.js'
 
 describe('files usage stats component', () => {
 	it('displays a spending bar with four sub-bars', () => {
-		const component = shallow(<UsageStats allowance={100} downloadspending={10} uploadspending={10} storagespending={10} contractspending={10} />)
+		const component = shallow(<UsageStats allowance={100} downloadspending={10} uploadspending={10} storagespending={10} contractspending={10} unspent={10} renewheight={16000} />)
 		expect(component.find('.spending-bar')).to.have.length(1)
 		expect(component.find('.spending-bar').children()).to.have.length(4)
 	})
 	it('renders subbars with correct width attributes', () => {
-		const component = shallow(<UsageStats allowance={100} downloadspending={5} uploadspending={5} storagespending={20} contractspending={10} />)
+		const component = shallow(<UsageStats allowance={100} downloadspending={5} uploadspending={5} storagespending={20} contractspending={10} unspent={10} renewheight={16000} />)
 		expect(component.find('.download-spending').props().style).to.have.property('width', (5/40)*100+ '%')
 		expect(component.find('.upload-spending').props().style).to.have.property('width', (5/40)*100+ '%')
 		expect(component.find('.storage-spending').props().style).to.have.property('width', (20/40)*100+ '%')
